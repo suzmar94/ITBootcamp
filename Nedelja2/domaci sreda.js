03be78b286d423142cd366280a3a42dcd433da74
suzmar94 <suzmar94@gmail.com>
2020-07-22 17:00:45 +0200

    izmena

--- Nedelja2/domaci sreda.js
+++ Nedelja2/domaci sreda.js
@@ -51,7 +51,3 @@ let a = 40, b = 50;
     // for (let brojDeljivih = 1000, i=0; brojDeljivih >0; brojDeljivih--) {
     //   console.log(i+=5);
     // }
-    
-// for (let brojDeljivih = 1000, i=0; brojDeljivih >0; brojDeljivih--) {
-    //   console.log(i+=5);
-    // }
