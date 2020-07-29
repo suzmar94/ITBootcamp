9546a050dfc4b373525af0fcc1fbb6857fb48eba
suzmar94 <suzmar94@gmail.com>
2020-07-29 14:50:28 +0200

    izmena

---
+++ Nedelja3/domaciSreda.js
@@ -0,0 +1,21 @@
+// /Написати функцију која враћа максимум од 3 броја (Параметри функције су 3 броја)
+
+function maxNumber(a, b, c){
+    if(a>b && a>c){
+        return a;
+    }else if(b> a && b>c){
+        return b;
+    }else if(c>a && c>b){
+        return c;
+    }
+}
+console.log(maxNumber(5, 3, 14));
+
+
+//Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм, и цена пице)
+
+function cenaPicePoCmKvadratnom(r, cenaPice) {
+    let P = Math.pow(r, 2) * Math.PI;
+    console.log(`cena pice po kvadratnom centimetru je ${cenaPice/P}`);
+}
+cenaPicePoCmKvadratnom(5, 850);
